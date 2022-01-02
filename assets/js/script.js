579991e6342aaeb8a6a19ba17e861312973da916
deagustinchristian <deagustinchristian@gmail.com>
2022-01-02 21:30:45 +0000

    Add variable to JS that I will use

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+/**
+ * Variables that represent the elements in my document that I will use
+ */
 const restartBtn = document.getElementById("restart");
 const prevBtn = document.getElementById("prev");
 const nextBtn = document.getElementById("next");
+const submitBtn = document.getElementById("submit");
+const trueBtn = document.getElementById("True");
+const falseBtn = document.getElementById("False");
+const userScore = document.getElementById("user-score");
+const totalScore = document.getElementById("total-score");
+const questionText = document.getElementById("question-text");
